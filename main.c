9543768b30d32a8047b9eff5b2d00bd9e81c664d
cppgirl <nataliaswierzynska0@gmail.com>
2026-06-28 21:32:04 +0200

    added a window!!!!!! :D

--- main.c
+++ main.c
@@ -3,8 +3,56 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-    printf("Hello World\n");
+void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
-    return 0;
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+GLFWwindow *initGLFW() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *result = glfwCreateWindow(800, 600, "Window Tittle", NULL, NULL);
+
+    if (result != NULL) {
+        glfwMakeContextCurrent(result);
+    }
+
+    return result;
+}
+
+#define IF(x, y) if (!(x)) { printf("%s\n", y); } else
+
+int main() {
+    GLFWwindow *window = initGLFW();
+    int result = 1;
+
+    IF (NULL != window, "No Window")
+    IF (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "No GLAD") {
+        result = 0;
+
+        glViewport(0, 0, 800, 600);
+        glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+        while (!glfwWindowShouldClose(window)) {
+            processInput(window);
+
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    glfwTerminate();
+
+    return result;
 }
