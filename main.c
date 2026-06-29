1eec9871334bb875960830164c93aaf9e44b1317
cppgirl <nataliaswierzynska0@gmail.com>
2026-06-29 16:52:50 +0200

    TRIANGLE!!!!!!

--- main.c
+++ main.c
@@ -33,8 +33,9 @@ static char *copyFileContent(const char *filename, int *lines) {
     return buffer;
 }
 
-void shaderInit() {
+void shaderInit(unsigned int shaderProgram) {
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     int size = 0;
     //ugly file path to be fixed
     char *vertexShaderSource = copyFileContent("C:/Users/mango/Desktop/JFA The Game/shader.vert", &size);
@@ -42,14 +43,27 @@ void shaderInit() {
     glShaderSource(vertexShader, 1, (const char **)&vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
+    char *vertexShaderSource2 = copyFileContent("C:/Users/mango/Desktop/JFA The Game/shader.frag", &size);
+    glShaderSource(fragmentShader, 1, (const char **)&vertexShaderSource2, NULL);
+    glCompileShader(fragmentShader);
+
     int success;
     char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("Vertex shader compilation failed :(\n");
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::SHADER::PROGRAM::LINKING_FAILED\n %s\n", infoLog);
     }
 
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
@@ -88,6 +102,8 @@ int main() {
     unsigned int VBO;
     GLFWwindow *window = initGLFW();
     int result = 1;
+    unsigned int VAO;
+    unsigned int shaderProgram;
 
     IF (NULL != window, "No Window")
     IF (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "No GLAD") {
@@ -96,21 +112,40 @@ int main() {
         glViewport(0, 0, 800, 600);
         glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+        shaderProgram = glCreateProgram();
+        shaderInit(shaderProgram);
+
+        glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
+        glBindVertexArray(VAO);
+
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        shaderInit();
-        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindVertexArray(0);
+
         while (!glfwWindowShouldClose(window)) {
             processInput(window);
 
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
+
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteProgram(shaderProgram);
     }
 
     glfwTerminate();
--- shader.frag
+++ shader.frag
@@ -1,1 +1,8 @@
-#version 330 core
+#version 400 core
+
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+
